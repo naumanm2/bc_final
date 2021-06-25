5713804b04c66255ed332bbe5153006c25b9df44
naumanm2 <max.naumanen@aalto.fi>
2021-06-25 19:37:32 +0300

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const siteUrl = `https://burgercompany.fi`
 
 module.exports = {
   siteMetadata: {
@@ -21,53 +20,10 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     {
-      resolve: "gatsby-plugin-sitemap",
+      resolve: `gatsby-plugin-sitemap`,
       options: {
         output: "/",
-        query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
-          }
-        }
-      `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node;
-            acc[uri] = node;
-
-            return acc;
-          }, {});
-
-          return allPages.map((page) => {
-            return { ...page, ...wpNodeMap[page.path] };
-          });
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
-          };
-        },
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-google-gtag`,
